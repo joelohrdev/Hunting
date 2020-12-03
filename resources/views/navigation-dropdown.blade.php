0a1dfe034df9cca8d72b0ae4e8bd600a13e35f72
jltkd <jl@turnkeydigital.com>
2020-12-03 14:23:22 -0600

    Added Permit Route and Index View

--- resources/views/navigation-dropdown.blade.php
+++ resources/views/navigation-dropdown.blade.php
@@ -16,6 +16,11 @@
                         {{ __('Dashboard') }}
                     </x-jet-nav-link>
                 </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-jet-nav-link href="{{ route('permits') }}" :active="request()->routeIs('permit')">
+                        {{ __('Permits') }}
+                    </x-jet-nav-link>
+                </div>
             </div>
 
             <!-- Settings Dropdown -->
